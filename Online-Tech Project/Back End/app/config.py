d79c7182c92fdd5616c781e8daed68c8bfe55145
ThomCarlos0913 <39784366+ThomCarlos0913@users.noreply.github.com>
2018-12-08 11:36:09 +0800

    Update config.py

--- Online-Tech Project/Back End/app/config.py
+++ Online-Tech Project/Back End/app/config.py
@@ -1,6 +1,6 @@
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@192.168.99.100:3308/OnlineTechDataStore'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@backend/OnlineTechDataStore'
     USER_TABLENAME = 'users_account'
     EVENT_TABLENAME = 'event_details'
     FEATURED_TABLENAME = 'featured_event'
